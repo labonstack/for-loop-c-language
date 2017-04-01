77604f77dd5c3f1ebdcf2ef8d32511055b465aa5
Khodidas Diyora <khodidasdiyora@gmail.com>
2017-04-01 13:55:02 +0530

    Add files via upload

---
+++ for-loop-c-language.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*This is an example of a for loop*/
+void main()
+{
+   int index;
+   for(index = 0;index < 6;index = index + 1)
+   {
+        printf("The value of the index is %d\n",index);
+   }
+}
